4096eb6860fc03ed66c5f679bc97a5a098624364
Nathan <nph29@drexel.edu>
2020-06-08 00:20:13 -0400

    Moved images and fonts into .tt ContentLoader

---
+++ Isometric/ContentLoader1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Isometric
+{
+
+	/// <summary>
+	/// Automatically loads images and fonts from the Content project
+	/// TEXT-TEMPLATE GENERATED, DO NOT EDIT MANUALLY
+	/// To re-generate, right click the .tt file and select "Run Custom tool"
+	///   Or click Build->"Transform All T4 Templates"
+	/// </summary>
+	static class ContentLoader
+	{
+		public static Texture2D altSelector { get; private set; }
+		public static Texture2D iso_land { get; private set; }
+		public static Texture2D plains { get; private set; }
+		public static Texture2D selector { get; private set; }
+		public static Texture2D snow { get; private set; }
+		public static Texture2D UDT_Aggressor { get; private set; }
+		public static SpriteFont Arial { get; private set; }
+		
+		public static void Initialize(ContentManager cm)
+		{
+
+			altSelector = cm.Load<Texture2D>(@"altSelector");
+			iso_land = cm.Load<Texture2D>(@"iso_land");
+			plains = cm.Load<Texture2D>(@"plains");
+			selector = cm.Load<Texture2D>(@"selector");
+			snow = cm.Load<Texture2D>(@"snow");
+			UDT_Aggressor = cm.Load<Texture2D>(@"UDT_Aggressor");
+			Arial = cm.Load<SpriteFont>(@"Arial");
+		}
+	}
+}
--- Isometric/Game1.cs
+++ Isometric/Game1.cs
@@ -12,21 +12,15 @@ namespace Isometric
         SpriteBatch spriteBatch;
         int worldWidth;
         int worldHeight;
-        Texture2D selector;
-        Texture2D altSelector;
-        Texture2D plains;
         TileType plainsTT;
-        Texture2D snow;
         TileType snowTT;
         Tile[,] world;
         UnitType aggressorUT;
-        Texture2D aggressor;
         List<Unit> units;
         Vector2 cameraOffset;
         Vector2 selectedTile;
         Vector2 lockedTile = new Vector2(-1,-1);
         int selectedUnitIndex = -1;
-        SpriteFont font;
         bool spacer;
         int turn;
         public Game1()
@@ -51,17 +45,12 @@ namespace Isometric
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            plains = Content.Load<Texture2D>("plains");
-            snow = Content.Load<Texture2D>("snow");
-            aggressor = Content.Load<Texture2D>("UDT_Aggressor");
-            selector = Content.Load<Texture2D>("selector");
-            altSelector = Content.Load<Texture2D>("altSelector");
+            ContentLoader.Initialize(Content);
 
-            aggressorUT = new UnitType("Aggressor", "UE", "The “Aggressor” light tank sacrifices firepower for superior armor, granting it the ‘medium’ armor classification- though it is still without most traits of a main battle tank. While this unit’s rapid fire main cannon is effective against soft targets, it lacks armor penetration and is at a disadvantage against equally armored foes.", "Light Tank", 1, 2200, 0, 0, 338, 200, "Medium", new Weapon[1] { new Weapon(30, "Pierce", 8, 2, 0) }, 8, "tread", 8, aggressor);
-            plainsTT = new TileType("plains", "Plains", "An area of primarily open and flat plains.", plains, 500, "plains_damage_1", 1, 1, 1, 0, 2, 1, 2, 2, 2, 2, 1, 1, 0, 0, 0, 0);
-            snowTT = new TileType("snow", "Snow", "An area of permafrost covered permanently by deep snow.", snow, 2700, "tundra_battlefield", 1, 0.75f, 0.73f, 0, 5, 6, 6, 4, 6, 2, 1, 1, 0, 0, 0, 0);
+            aggressorUT = new UnitType("Aggressor", "UE", "The “Aggressor” light tank sacrifices firepower for superior armor, granting it the ‘medium’ armor classification- though it is still without most traits of a main battle tank. While this unit’s rapid fire main cannon is effective against soft targets, it lacks armor penetration and is at a disadvantage against equally armored foes.", "Light Tank", 1, 2200, 0, 0, 338, 200, "Medium", new Weapon[1] { new Weapon(30, "Pierce", 8, 2, 0) }, 8, "tread", 8, ContentLoader.UDT_Aggressor);
+            plainsTT = new TileType("plains", "Plains", "An area of primarily open and flat plains.", ContentLoader.plains, 500, "plains_damage_1", 1, 1, 1, 0, 2, 1, 2, 2, 2, 2, 1, 1, 0, 0, 0, 0);
+            snowTT = new TileType("snow", "Snow", "An area of permafrost covered permanently by deep snow.", ContentLoader.snow, 2700, "tundra_battlefield", 1, 0.75f, 0.73f, 0, 5, 6, 6, 4, 6, 2, 1, 1, 0, 0, 0, 0);
 
-            font = Content.Load<SpriteFont>("Arial");
 
             for (int i = 0; i < worldHeight; i++)
             {
@@ -232,24 +221,24 @@ namespace Isometric
                         spriteBatch.Draw(world[p,i].getType().getSprite(), world[p,i].getScreenPosition());
                     }
                 }
-                spriteBatch.Draw(selector, new Vector2((selectedTile.X * 64 - selectedTile.Y * 64)-4, (selectedTile.X * 32 + selectedTile.Y * 32)-4));
+                spriteBatch.Draw(ContentLoader.selector, new Vector2((selectedTile.X * 64 - selectedTile.Y * 64)-4, (selectedTile.X * 32 + selectedTile.Y * 32)-4));
                 if ( lockedTile.X!=-1 && lockedTile.Y != -1)
                 {
-                    spriteBatch.Draw(altSelector, new Vector2((lockedTile.X * 64 - lockedTile.Y * 64) - 4, (lockedTile.X * 32 + lockedTile.Y * 32) - 4));
+                    spriteBatch.Draw(ContentLoader.altSelector, new Vector2((lockedTile.X * 64 - lockedTile.Y * 64) - 4, (lockedTile.X * 32 + lockedTile.Y * 32) - 4));
                 }
             foreach (Unit unit in units)
             {
                 spriteBatch.Draw(unit.getUnitType().getSprite(), new Vector2(unit.getPosition().X*64 - unit.getPosition().Y*64, unit.getPosition().X*32 + unit.getPosition().Y*32));
                 if (unit.getMoves() == 0)
                 {
-                    spriteBatch.DrawString(font, "z", new Vector2((unit.getPosition().X * 64 - unit.getPosition().Y * 64)+32, unit.getPosition().X * 32 + unit.getPosition().Y * 32),Color.Black);
+                    spriteBatch.DrawString(ContentLoader.Arial, "z", new Vector2((unit.getPosition().X * 64 - unit.getPosition().Y * 64)+32, unit.getPosition().X * 32 + unit.getPosition().Y * 32),Color.Black);
                 }
             }
             if (selectedUnitIndex != -1)
             {
-                spriteBatch.DrawString(font, "Remaining Moves: "+units[selectedUnitIndex].getMoves().ToString(), new Vector2(10, 30)-cameraOffset, Color.Red);
+                spriteBatch.DrawString(ContentLoader.Arial, "Remaining Moves: "+units[selectedUnitIndex].getMoves().ToString(), new Vector2(10, 30)-cameraOffset, Color.Red);
             }
-            spriteBatch.DrawString(font, "Turn " + turn, new Vector2(10, 10) - cameraOffset, Color.Red);
+            spriteBatch.DrawString(ContentLoader.Arial, "Turn " + turn, new Vector2(10, 10) - cameraOffset, Color.Red);
             spriteBatch.End();
             
             base.Draw(gameTime);
